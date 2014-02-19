df752cbae5586576d3c25e6a8fe7a44bec86371f
Joseph Finlayson <joseph.finlayson@accenture.com>
2014-02-19 14:25:40 +0000

    add satoshi info

--- js/main.js
+++ js/main.js
@@ -3,25 +3,30 @@ jQuery(document).ready(function($) {
 	jQuery.get('https://api.bitcoinaverage.com/all', '' , function(response, textStatus, xhr) {
 	//optional stuff to do after success
 
-	console.log(response);
-	console.log(response.USD.last)
-	yesterdollars = response.USD.averages['24h_avg']
-	lastdollars   = response.USD.averages.last
-	jQuery('#btcprice').html("$ "+response.USD.averages.last+" dollars")
+		console.log(response);
+		console.log(response.USD.last)
+		yesterdollars = response.USD.averages['24h_avg']
+		lastdollars   = response.USD.averages.last
+		jQuery('#btcprice').html("$"+response.USD.averages.last+" USD")
 
 
-	difference = lastdollars - yesterdollars
-	if (difference > 0) {
-		word = " more "
-		style = 'green'
-	} else {
-		word = " less "
-		style = 'red'
-	}
-	rounded = Math.round(Math.abs(difference)*100)/100
+		difference = lastdollars - yesterdollars
+		if (difference > 0) {
+			word = " more "
+			style = 'green'
+		} else {
+			word = " less "
+			style = 'red'
+		}
+		rounded = Math.round(Math.abs(difference)*100)/100
 
-	$('#yesterdays_price').html(
-		'This is $'+rounded+'<span id="word" style="color:'+style+'">'+ word +'</span> than yesterday'
+		$('#yesterdays_price').html(
+			'This is $'+rounded+'<span id="word" style="color:'+style+'">'+ word +'</span> than yesterday'
 		);
+		satoshi = lastdollars/100000000
+		$('#satoshi').html(
+			satoshi
+		);
+
 	});
 });
